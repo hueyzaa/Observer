e4030e00f94f84d6a16c971c612dfe0e27e3b2cd
Hueyzaa <giahuy14sp@gmail.com>
2025-10-01 10:59:33 +0700

    update code

--- ObserverPattern/obj/Debug/net9.0/ObserverPattern.AssemblyInfo.cs
+++ ObserverPattern/obj/Debug/net9.0/ObserverPattern.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("ObserverPattern")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+10a20b89822789ca95cf4c32189280509ec028d1")]
 [assembly: System.Reflection.AssemblyProductAttribute("ObserverPattern")]
 [assembly: System.Reflection.AssemblyTitleAttribute("ObserverPattern")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
